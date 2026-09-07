6e999771f0a88471334dbc67c5ee8c47cef2b086
Muhammad Abdullah <abdullahahmad3744@gmail.com>
2026-09-07 13:10:21 +0500

    routes or end points for user

--- 02-Backend/09-CRUD Operation/index.js
+++ 02-Backend/09-CRUD Operation/index.js
@@ -19,10 +19,12 @@ app.get('/',(req,res,) => {
     res.status(200).send('Welcome to Home Page')
 })
 
+ 
 // Get request for all food items 
 app.get('/food',(req,res) => {
     res.status(200).send(foodMenu)
 })
+
 
 // Post request for adding the items
 app.post('/admin',(req,res) => {
@@ -43,12 +45,12 @@ app.delete('/admin/:id',(req,res) => {
         }
 })
 
+
 // Patch request for an update in app
 app.patch('/admin',(req,res) => {
         const id = parseInt(req.body.id);
         const targetItem = foodMenu.find(item => item.id === id)
         if (targetItem) {
-            // console.log("Target Item before updation : ", targetItem);
             if (req.body.food) {
                 targetItem.food = req.body.food
             }
@@ -58,12 +60,50 @@ app.patch('/admin',(req,res) => {
             if (req.body.price) {
                 targetItem.price = req.body.price
             }
-            // console.log("Target item after updation ",targetItem);
             res.status(200).send('Pacth applied')
         } else {
             res.status(404).send('Item Not Found')
         }
 })
+
+
+// User viewing cart Items
+app.get('/user',(req,res) => {
+    if (userAddToCart.length > 0 ) {
+        res.status(200).send(userAddToCart);
+    } else{
+        res.status(401).send('Cart is Empty')
+    }
+    
+})
+
+
+// User adding items to the cart 
+app.post('/user/:id',(req,res) => {
+    const id = parseInt(req.params.id)
+    const targetItem = foodMenu.find(item => item.id === id)
+    if (targetItem) {
+        userAddToCart.push(targetItem)
+        res.status(200).send('Item added to cart ')
+    } else {
+        res.status(400).send('Item out of stock')
+    }
+})
+
+
+// Removing an Item from User Add to Cart 
+app.delete('/user/:id',(req,res) => {
+    const id = parseInt(req.params.id);
+    if (userAddToCart.length > 0) {
+        const updatedUserAddToCart = userAddToCart.filter(item => item.id !== id)
+        console.log(updatedUserAddToCart);
+        res.status(200).send('Item removed from cart')
+    } else{
+        res.send("Cart is Empty")
+    }
+})
+
+
 // Server listening
 app.listen(PORT,() => {
     console.log(`App is listening at PORT : ${PORT}`);

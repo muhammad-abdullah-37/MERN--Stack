b6616fabd595d19f7522b6cb2a24b65194d7d4a1
Muhammad Abdullah <abdullahahmad3744@gmail.com>
2026-09-09 18:34:45 +0500

    CRUD operation completed using mongoose

--- 02-Backend/11-Mongoose/index.js
+++ 02-Backend/11-Mongoose/index.js
@@ -4,6 +4,7 @@ require('dotenv').config({quiet : true});
 require('./database.js')
 const main = require('./database');
 const User = require('./models/users.js');
+const { set } = require('mongoose');
 
 const PORT = parseInt(process.env.PORT) || 5000;
 
@@ -11,27 +12,47 @@ app.use(express.json())
 
 // Get request for Getting all data
 app.get('/info', async (req,res) => {
+   try {
     const data = await User.find({});
     res.send(data)
+   } catch (error) {
+    console.log('Error in getting data from DB: ', error);
+   }
 });
 
 // Post request for adding new data
 app.post('/info', async (req,res) => {
-    const user1 = await User.create({
-        name : req.body.name,
-        age : req.body.age,
-        city: req.body.city,
-        gender : req.body.gender
-    })
-    res.send('Data added successfully')
+    try {
+        const user1 = new User(req.body)
+        await user1.save()
+        // using create method
+        // await User.create(req.body)
+        res.send('Data added successfully')
+    } catch (error) {
+        console.log(`Error in resourcr creation : `, error);
+    }
 })
 
 // Delete request for deleting a resource
 app.delete('/info/:id', async (req,res) => {
-    const id  = req.params.id;
-    await User.findByIdAndDelete(id);
+    try {
+        const id  = req.params.id;
+        await User.findByIdAndDelete(id);
+        res.send('Data deleted successfully ')
+    } catch (error) {
+        console.log('Error in deleting the resourcr: ',error);
+    }
+})
 
-    res.send('Data deleted successfully ')
+// patch request for updating the resource
+app.put('/info/:id', async (req,res) => {
+    try {
+        const updatedUser = await User.updateOne({_id : req.params.id},
+            {$set : {name  :  req.body.name, city : req.body.city}});
+            res.send('Patches applied')
+    } catch (error) {
+        console.log('Error in updating the resource: ', error);
+    }
 })
 
 

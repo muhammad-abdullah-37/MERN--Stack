738455e178823e423428e18aec845bc8f3da0520
Muhammad Abdullah <abdullahahmad3744@gmail.com>
2026-09-09 12:05:11 +0500

    Basic MongoDB operations

--- 02-Backend/10-MongoDB/database.js
+++ 02-Backend/10-MongoDB/database.js
@@ -1,24 +1,39 @@
 const { MongoClient } = require('mongodb');
-require('dotenv').config()
-// or as an es module:
-// import { MongoClient } from 'mongodb'
+require('dotenv').config();
 
 // Connection URL
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGO_URI;
 const client = new MongoClient(url);
 
 // Database Name
-const dbName = 'myProject';
+const dbName = 'MERN-Stack';
 
 async function main() {
   // Use connect method to connect to the server
   await client.connect();
   console.log('Connected successfully to server');
   const db = client.db(dbName);
-  const collection = db.collection('documents');
+  const collection = db.collection('user');
 
   // the following code examples can be pasted here...
 
+  // const findResult = collection.find({});
+  // for (const doc of findResult) {
+  //   console.log(doc);
+  // }
+
+  // Getting data from the Mongodb cluster
+  const findResult = await collection.find({}).toArray();
+  console.log('Found documents =>', findResult);
+
+  // Inserting data in the cluster
+  // Inserting one document 
+  // const insertResult = await collection.insertOne({name : 'Shahazaib Khadim', city : 'Kashmir', age : 22})
+  // console.log('Inserted documents => ', insertResult);
+
+  // Inserting many documents at once
+  // const insertMany = await collection.insertMany([{name : 'Areeb Khan', city : "Bahawalpur", age : 22}, {name : 'Ghasif Shahzad', city : 'Lahore', age : 23}, {name : 'Hamza', city : 'Dera Ismail Khan', age : 23}])
+  // console.log(insertMany);
   return 'done.';
 }
 

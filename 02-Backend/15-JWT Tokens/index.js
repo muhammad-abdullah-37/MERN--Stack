e1fbb86e00573c021a5a314df7c000f290062b7e
Muhammad Abdullah <abdullahahmad3744@gmail.com>
2026-09-12 16:03:06 +0500

    JWT tokens and Cookie

--- 02-Backend/15-JWT Tokens/index.js
+++ 02-Backend/15-JWT Tokens/index.js
@@ -7,23 +7,46 @@ const PORT = process.env.PORT || 5000;
 const main = require('./database.js')
 const User = require('./models/users.js')
 const validateUsers = require('./utils/validateUser.js')
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 
 // DATA Parsing using middleware;
 app.use(express.json());
+app.use(cookieParser());
 
 //default Route
 app.get('/',(req,res) => {
     res.send('Welcome To Home Page for Learning JWT Tokens')
 })
+
+
 // Get request for Fetching All Users
-app.get('/users', async (req,res) => {
+app.get('/info', async (req,res) => {
     try {
+       
         const allUsers = await User.find();
         res.send(allUsers);
     } catch (error) {
         res.send(`Error in Fetching Users : ${error.message}`)
     }
 })
+
+
+
+// getting a user based on ID 
+app.get('/user', async (req,res) => {
+    try {
+          // Validating user by token 
+        const payload = jwt.verify(req.cookies.token, 'Abdullah@123')
+        // getting a user by id
+        const targetUser  = await User.findById(payload._id);
+        res.send(targetUser);
+    } catch (error) {
+        res.send(`Error in User Fetching : ${error.message} `)
+    }
+})
+
+
 // POST request for creating a user
 app.post('/register', async (req, res) => {
     try {
@@ -40,20 +63,49 @@ app.post('/register', async (req, res) => {
        
     }
 })
+
+
+// Delete request for Deleting a user
+app.delete('/delete', async (req,res) => {
+    try {
+        await User.findByIdAndDelete(req.body._id);
+        res.send('User Deleted')
+    } catch (error) {
+        res.send(`Error in User Deletion : ${error.message}`)
+    }
+})
+
+
+// Put request for Updating the user
+app.put('/update', async (req,res) => {
+    try {
+        const {_id, ...update} = req.body
+        await User.findByIdAndUpdate(_id,update,{runValidators : true});
+        res.send('User Updated Successfully')
+    } catch (error) {
+        res.send(`Error in User Updation : ${error.message}`)
+    }
+
+})
+
+
 // Post request for User Login
 app.post('/login', async(req,res) => {
     try {
         // Finding user by id for login
-        const targetUser = await User.findById(req.body._id);
+        const targetUser = await User.findOne({emailId : req.body.emailId});
         // Email verification for Login
         if (!(req.body.emailId == targetUser.emailId)) {
-            throw new Error('Invalid Email')
+            throw new Error('Invalid Email') 
         }
         // Password verification for Login
         const isLoginAllowed = await bcrypt.compare(req.body.password, targetUser.password);
         if (!isLoginAllowed) {
             throw new Error('Invalid Password')
         }
+        //JWT Token
+        const token = jwt.sign({_id : targetUser._id, emailId : targetUser.emailId},'Abdullah@123', {expiresIn : 10})
+        res.cookie("token", token)
         res.send('Logged In Successfully ')
     } catch (error) {
         res.send(`Error in User Login : ${error.message}`);

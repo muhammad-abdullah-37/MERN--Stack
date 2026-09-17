da5587111dd6f560320ff6c830243a7924e6f285
Muhammad Abdullah <abdullahahmad3744@gmail.com>
2026-09-18 00:41:17 +0500

    UI Improved

--- 01-Frontend/04-React/15-Random GIF Generator Project-2/src/App.jsx
+++ 01-Frontend/04-React/15-Random GIF Generator Project-2/src/App.jsx
@@ -3,7 +3,7 @@ import Random from "./components/Random";
 import Tag from "./components/Tag";
 function App() {
   return (
-    <div className="w-full h-screen flex flex-col background relative overflow-x-hidden items-center">
+    <div className="w-full h-screen flex flex-col background relative overflow-x-hidden items-center pb-10">
       <h1 className="bg-white rounded-lg w-11/12 text-center mt-[30px] px-10 py-1 text-2xl font-bold">Random GIFS</h1>
       <div className="flex flex-col w-full items-center gap-y-10 mt-[20px]">
         <Random />
